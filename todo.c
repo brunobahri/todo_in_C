41298f10436fbb1f73eccdb1a384573f09ca000b
brunobahri <brunobahri27@gmail.com>
2025-01-10 20:42:54 -0300

    feat: alpha 0.01 release

--- todo.c
+++ todo.c
@@ -432,7 +432,7 @@ rendernewtask() {
     props.corner_radius = 11;
     props.text_color = LF_WHITE;
     props.border_width = 1.0f;
-    props.border_color = s.new_taslfk_input.selected ? LF_WHITE : (LfColor){170, 170, 170, 255};
+    props.border_color = s.new_task_input.selected ? LF_WHITE : (LfColor){170, 170, 170, 255};
     props.corner_radius = 2.5f;
     props.margin_bottom = 10.0f;
     lf_push_style_props(props);
@@ -444,6 +444,7 @@ rendernewtask() {
 
   lf_next_line();
 
+  // Priority dropdown
   static int32_t selected_priority = -1;
   {
     lf_push_font(&s.smallfont);
@@ -456,4 +457,485 @@ rendernewtask() {
       "Medium",
       "High"
     };
+    static bool opened = false;
+    LfUIElementProps props = lf_get_theme().button_props;
+    props.color = (LfColor){70, 70, 70, 255};
+    props.text_color = LF_WHITE;
+    props.border_width = 0.0f;
+    props.corner_radius = 5.0f;
+    lf_push_style_props(props);
+    lf_dropdown_menu(items, "Priority", 3, 200, 80, &selected_priority, &opened);
+    lf_pop_style_props();
   }
+
+  // Add Button
+  {
+    bool form_complete = (strlen(s.new_task_input_buf) && selected_priority != -1);
+    const char* text = "Add";
+    const float width = 150.0f;
+
+    LfUIElementProps props = lf_get_theme().button_props;
+    props.margin_left = 0.0f;
+    props.margin_right = 0.0f;
+    props.corner_radius = 5.0f;
+    props.border_width = 0.0f;
+    props.color = !form_complete ? (LfColor){80, 80, 80, 255} :  SECONDARY_COLOR; 
+    lf_push_style_props(props);
+    lf_set_line_should_overflow(false);
+    lf_set_ptr_x_absolute(s.winw - (width + lf_get_theme().button_props.padding * 2.0f) - GLOBAL_MARGIN);
+    lf_set_ptr_y_absolute(s.winh - (lf_button_dimension(text).y + lf_get_theme().button_props.padding * 2.0f) - GLOBAL_MARGIN);
+
+    // If the user wants to add a new task: 
+    if((lf_button_fixed(text, width, -1) == LF_CLICKED && form_complete) ||
+      (lf_key_went_down(GLFW_KEY_ENTER) && form_complete)) {
+
+      // Copy the description input buffers content to a new pointer
+      char* desc = malloc(strlen(s.new_task_input_buf));
+      strcpy(desc, s.new_task_input_buf);
+
+      // Allocate a new entry
+      todo_entry* entry = malloc(sizeof(todo_entry));
+      entry->desc = desc;
+      entry->date = get_command_output(DATE_CMD);
+      entry->completed = false;
+      entry->priority = (entry_priority)selected_priority;
+      entries_da_push(&s.todo_entries, entry);
+      sort_entries_by_priority(&s.todo_entries);
+
+      // Serialize entries 
+      serialize_todo_list(s.tododata_file, &s.todo_entries);
+
+      // Reset interface state
+      memset(s.new_task_input_buf, 0, sizeof(s.new_task_input_buf));
+      s.new_task_input.cursor_index = 0;
+      lf_input_field_unselect_all(&s.new_task_input);
+    }
+    lf_set_line_should_overflow(true);
+    lf_pop_style_props();
+  }
+
+  // Back Icon button
+  lf_next_line();
+  {
+    LfUIElementProps props = lf_get_theme().button_props;
+    props.color = LF_NO_COLOR;
+    props.border_width = 0.0f;
+    props.padding = 0.0f;
+    props.margin_left = 0.0f;
+    props.margin_right = 0.0f;
+    props.margin_top = 0.0f;
+    props.margin_bottom = 0.0f;
+    lf_push_style_props(props);
+    lf_set_line_should_overflow(false);
+    LfTexture backbutton = (LfTexture){.id = s.backicon.id, .width = 20, .height = 40};
+    lf_set_ptr_y_absolute(s.winh - backbutton.height - GLOBAL_MARGIN * 2.0f);
+    lf_set_ptr_x_absolute(GLOBAL_MARGIN);
+
+    if(lf_image_button(backbutton) == LF_CLICKED) {
+      s.crnt_tab = TAB_DASHBOARD;
+    }
+    lf_set_line_should_overflow(true);
+    lf_pop_style_props();
+  }
+}
+void 
+entries_da_init(entries_da* da) {
+  da->cap = DA_INIT_CAP;
+  da->count = 0;
+  da->entries = (todo_entry**)malloc(sizeof(todo_entry) * da->cap);
+}
+
+void 
+entries_da_push(entries_da* da, todo_entry* entry) {
+  if(da->count == da->cap) {
+    entries_da_resize(da, da->cap * 2);
+  }
+  da->entries[da->count++] = entry;
+}
+
+void 
+entries_da_resize(entries_da* da, int32_t new_cap) {
+    todo_entry** temp = (todo_entry**)realloc(da->entries, new_cap * sizeof(todo_entry));
+    if (!temp) {
+        fprintf(stderr, "Failed to reallocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    da->entries = temp;
+    da->cap = new_cap;
+}
+
+void 
+entries_da_remove_i(entries_da* da, uint32_t i) {
+  // Bounds check 
+  if (i < 0 || i >= da->count) {
+    printf("Index out of bounds\n");
+    return;
+  }
+
+  // Remove element
+  for (uint32_t idx = i; idx < da->count - 1; idx++) {
+    da->entries[idx] = da->entries[idx + 1];
+  }
+
+  // Decrease the count
+  da->count--;
+}
+
+void entries_da_free(entries_da* da) {
+  if(da->entries)
+    free(da->entries);
+  da->cap = 0;
+  da->count = 0;
+}
+
+int
+compare_entry_priority(const void* a, const void* b) {
+  todo_entry* entry_a = *(todo_entry**)a;
+  todo_entry* entry_b = *(todo_entry**)b;
+  return (entry_b->priority - entry_a->priority);
+}
+
+void 
+sort_entries_by_priority(entries_da* da) {
+  qsort(da->entries, da->count, sizeof(todo_entry*), compare_entry_priority);
+}
+
+char* 
+get_command_output(const char* cmd) {
+    FILE *fp;
+    char buffer[1024];
+    char *result = NULL;
+    size_t result_size = 0;
+
+    // Opening a new pipe with the fiven command
+    fp = popen(cmd, "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n");
+        return NULL;
+    }
+
+    // Reading the output
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        size_t buffer_len = strlen(buffer);
+        char *temp = realloc(result, result_size + buffer_len + 1);
+        if (temp == NULL) {
+            printf("Memory allocation failed\n");
+            free(result);
+            pclose(fp);
+            return NULL;
+        }
+        result = temp;
+        strcpy(result + result_size, buffer);
+        result_size += buffer_len;
+    }
+    pclose(fp);
+    return result;
+}
+
+void 
+serialize_todo_entry(FILE* file, todo_entry* entry) {
+  // Write completed to file
+  fwrite(&entry->completed, sizeof(bool), 1, file);
+
+  // Write description to file
+  size_t desc_len = strlen(entry->desc) + 1; // +1 for null terminator
+  // Writing the description length to the file for later 
+  // deserialization
+  fwrite(&desc_len, sizeof(size_t), 1, file);
+  fwrite(entry->desc, sizeof(char), desc_len, file);
+
+  // Write date to file
+  size_t date_len = strlen(entry->date) + 1; // +1 for null terminator
+  // Writing the date length to the file for later 
+  // deserialization
+  fwrite(&date_len, sizeof(size_t), 1, file);
+  fwrite(entry->date, sizeof(char), date_len, file);
+
+  // Write priority to file
+  fwrite(&entry->priority, sizeof(entry_priority), 1, file);
+}
+
+void
+serialize_todo_list(const char* filename, entries_da* da) {
+  FILE* file = fopen(filename, "wb");
+  if(!file) {
+    printf("Failed to open data file.\n");
+    return;
+  }
+  for(uint32_t i = 0; i < da->count; i++) {
+    serialize_todo_entry(file, da->entries[i]);
+  }
+  fclose(file);
+}
+
+todo_entry*  
+deserialize_todo_entry(FILE* file) {
+  // Allocate entry
+  todo_entry *entry = malloc(sizeof(todo_entry));
+
+  // Read if entry is completed
+  if (fread(&entry->completed, sizeof(bool), 1, file) != 1) {
+    free(entry);
+    return NULL;
+  }
+
+  // Read the length of the description
+  size_t desc_len;
+  if (fread(&desc_len, sizeof(size_t), 1, file) != 1) {
+    free(entry);
+    return NULL;
+  }
+  
+  // Allocating space to store the entries description
+  entry->desc = malloc(desc_len);
+  if (!entry->desc) {
+    free(entry);
+    return NULL;
+  }
+  // Read the description from the file
+  if (fread(entry->desc, sizeof(char), desc_len, file) != desc_len) {
+    free(entry->desc);
+    free(entry);
+    return NULL;
+  }
+
+  // Read the date length from the file
+  size_t date_len;
+  if (fread(&date_len, sizeof(size_t), 1, file) != 1) {
+    free(entry->desc);
+    free(entry);
+    return NULL;
+  }
+  // Allocating space for the date
+  entry->date = malloc(date_len);
+  if (!entry->date) {
+    free(entry->desc);
+    free(entry);
+    return NULL;
+  }
+  // Reading the date string
+  if (fread(entry->date, sizeof(char), date_len, file) != date_len) {
+    free(entry->desc);
+    free(entry->date);
+    free(entry);
+    return NULL;
+  }
+
+  // Reading the entires priority
+  if (fread(&entry->priority, sizeof(entry_priority), 1, file) != 1) {
+    free(entry->desc);
+    free(entry->date);
+    free(entry);
+    return NULL;
+  }
+
+  return entry;
+}
+void deserialize_todo_list(const char* filename, entries_da* da) {
+  FILE *file = fopen(filename, "rb");
+  if(!file) {
+    // If file does not exist, create it 
+    file = fopen(filename, "w");
+    fclose(file);
+  }
+  file = fopen(filename, "rb");
+  todo_entry *entry;
+  while ((entry = deserialize_todo_entry(file)) != NULL) {
+    entries_da_push(da, entry);
+  }
+  fclose(file);
+}
+
+void print_requires_argument(const char* option, uint32_t numargs) {
+  printf("todo: option requires %i argument(s): '%s'\n", numargs, option);
+  printf("Try todo --help for more information\n");
+}
+
+void str_to_lower(char* str) {
+  while(*str) {
+    *str = tolower((unsigned char)*str);
+    str++;
+  }
+}
+
+int 
+main(int argc, char** argv) {
+  // Handle terminal interface
+  if(argc > 1) {
+    initentries();
+    char* subcmd = argv[1];
+    str_to_lower(subcmd);
+    if(strcmp(subcmd, "--help") == 0 || strcmp(subcmd, "-h") == 0) {
+      printf("Usage: todo [OPTION...] [ARGUMENTS...]\n");
+      printf("\t-h, --help                        Open help menu\n");
+      printf("\t-l, --list                        Display todo list\n");
+      printf("\t-a, --add \"[desc]\" [priority]     Add a new task to the todo list\n");
+      printf("\t-r, --remove [idx]                Remove a task with a given index from the list.\n");
+      printf("\t-d, --done [idx]                  Mark a task with a given index as completed.\n");
+      printf("\t-n, --not-done [idx]              Mark a task with a given index as not completed.\n");
+      printf("\t-r, --raise [idx]                 Raises a task with a given index to the top.\n");
+    }
+    else if(strcmp(subcmd, "--list") == 0 || strcmp(subcmd, "-l") == 0) {
+      printf("======== Your To Do ========\n");
+      char* priorities_str[] = {
+        "L", "M", "H"
+      };
+      sort_entries_by_priority(&s.todo_entries);
+      for(uint32_t i = 0; i < s.todo_entries.count; i++) {
+        todo_entry* entry = s.todo_entries.entries[i];
+        printf("%i | (%s) [%c]: %s\n", i, priorities_str[entry->priority], entry->completed ? 'x' : ' ', entry->desc);
+      }
+      if(!s.todo_entries.count) {
+        printf("There is nothing here.\n");
+      }
+      printf("============================\n");
+    } 
+    else if(strcmp(subcmd, "--add") == 0 || strcmp(subcmd, "-a") == 0) {
+      if(argc < 4) {
+        print_requires_argument(argv[1], 2);
+        return EXIT_FAILURE;
+      }
+      char* desc = argv[2];
+      char* priority_str = argv[3];
+
+      str_to_lower(priority_str);
+
+      entry_priority priority;
+      if(strcmp(priority_str, "low") == 0) 
+        priority = PRIORITY_LOW;
+      else if(strcmp(priority_str, "medium") == 0) 
+        priority = PRIORITY_MEDIUM;
+      else if(strcmp(priority_str, "high") == 0)
+        priority = PRIORITY_HIGH;
+      else {
+        printf("todo: invalid priority given: '%s' (valid priorities: {low, medium, high})\n", priority_str);
+        return EXIT_FAILURE;
+      }
+      todo_entry* entry = (todo_entry*)malloc(sizeof(todo_entry));
+      entry->priority = priority;
+      entry->desc = desc;
+      entry->completed = false;
+      entry->date = get_command_output(DATE_CMD);
+
+      entries_da_push(&s.todo_entries, entry);
+      serialize_todo_list(s.tododata_file, &s.todo_entries);
+
+      printf("todo: added new entry to do list.\n");
+
+      free(entry);
+      entry = NULL;
+    }
+    else if(strcmp(subcmd, "--remove") == 0 || strcmp(subcmd, "-r") == 0) {
+      if(argc < 3) {
+        print_requires_argument(argv[1], 1);
+        return EXIT_FAILURE;
+      }
+      int32_t idx = atoi(argv[2]);
+      if(idx < 0 || idx >= s.todo_entries.count) {
+        printf("todo: index for removal out of bounds.\n");
+        return EXIT_FAILURE;
+      }
+      char* entry_desc = malloc(strlen(s.todo_entries.entries[idx]->desc));
+      strcpy(entry_desc, s.todo_entries.entries[idx]->desc);
+
+      entries_da_remove_i(&s.todo_entries, idx);
+      serialize_todo_list(s.tododata_file, &s.todo_entries);
+
+      printf("todo: removed item %i ('%s') from list.\n", idx, entry_desc);
+
+      free(entry_desc);
+      entry_desc = NULL;
+    }
+    else if(strcmp(subcmd, "--done") == 0 || strcmp(subcmd, "-d") == 0) {
+      if(argc < 3) {
+        print_requires_argument(argv[1], 1);
+        return EXIT_FAILURE;
+      }
+      int32_t idx = atoi(argv[2]);
+      if(idx < 0 || idx >= s.todo_entries.count) {
+        printf("todo: index for marking as done out of bounds.\n");
+        return EXIT_FAILURE;
+      }
+
+      todo_entry* entry = s.todo_entries.entries[idx];
+      entry->completed = true;
+      serialize_todo_list(s.tododata_file, &s.todo_entries);
+
+      printf("todo: marked item %i ('%s') as done.\n", idx, entry->desc);
+    }
+    else if(strcmp(subcmd, "--not-done") == 0 || strcmp(subcmd, "-n") == 0) {
+      if(argc < 3) {
+        print_requires_argument(argv[1], 1);
+        return EXIT_FAILURE;
+      }
+      int32_t idx = atoi(argv[2]);
+      if(idx < 0 || idx >= s.todo_entries.count) {
+        printf("todo: index for marking as not done out of bounds.\n");
+        return EXIT_FAILURE;
+      }
+
+      todo_entry* entry = s.todo_entries.entries[idx];
+      entry->completed = false;
+      serialize_todo_list(s.tododata_file, &s.todo_entries);
+
+      printf("todo: marked item %i ('%s') as not done.\n", idx, entry->desc);
+    }
+    else if(strcmp(subcmd, "--raise") == 0 || strcmp(subcmd, "-r") == 0) {
+      if(argc < 3) {
+        print_requires_argument(argv[1], 1);
+        return EXIT_FAILURE;
+      }
+      int32_t idx = atoi(argv[2]);
+      if(idx < 0 || idx >= s.todo_entries.count) {
+        printf("todo: index for raising out of bounds.\n");
+        return EXIT_FAILURE;
+      }
+
+      todo_entry* tmp = s.todo_entries.entries[0];
+      s.todo_entries.entries[0] = s.todo_entries.entries[idx];
+      s.todo_entries.entries[idx] = tmp;
+
+      serialize_todo_list(s.tododata_file, &s.todo_entries);
+
+      printf("todo: raised item %i ('%s') to the top.\n", idx, s.todo_entries.entries[0]->desc);
+    }
+    else {
+      printf("todo: invalid option: '%s'.\n", argv[1]);
+      printf("Try todo --help for more information.\n");
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  initwin();
+  initui();
+
+  vec4s bgcol = lf_color_to_zto(BG_COLOR);
+  while(!glfwWindowShouldClose(s.win)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(bgcol.r, bgcol.g, bgcol.b, bgcol.a);
+
+    lf_begin();
+   
+    // Beginning the root div
+    lf_div_begin(((vec2s){GLOBAL_MARGIN, GLOBAL_MARGIN}), ((vec2s){s.winw - GLOBAL_MARGIN * 2.0f, s.winh - GLOBAL_MARGIN * 2.0f}), true);
+
+    switch (s.crnt_tab) {
+      case TAB_DASHBOARD:
+        renderdashboard();
+        break;
+      case TAB_NEW_TASK:
+        rendernewtask();
+        break;
+    }
+
+    // Ending the root div 
+    lf_div_end();
+    lf_end();
+
+    glfwPollEvents();
+    glfwSwapBuffers(s.win);
+  }
+  terminate();
+  return EXIT_SUCCESS;
+}
